d449622641de4d1faa4f19cb862009c067649cdc
Carolina Dib <c.dib@correo.unimet.edu.ve>
2025-04-29 19:52:50 -0400

    ejercicio5

---
+++ ejercicio5.py
@@ -0,0 +1,8 @@
+numero1=input('Ingrese el primer numero: ')
+numero2=input('Ingrese el segundo numero: ')
+
+if int(numero1) >= int(numero2):
+    print(f'{int(numero1)/int(numero2)}')
+else:
+    print(f'{int(numero2)/int(numero1)}')
+    
---
+++ ejercicioprepa2.py
@@ -0,0 +1,6 @@
+#Crear un programa que intercambie el valor de dos variables 
+valora=input('Ingrese el valor del primer numero: ')
+valorb=input('Ingrese el valor del segundo numero: ')
+valora=int(valora)
+valorb=int(valorb)
+print(f'El valor de a es {valorb} y el valor de b es {valora}')
