4197895e51c05a236c3e6bbab10604e83c9a3b50
Carolina Dib <c.dib@correo.unimet.edu.ve>
2025-04-28 20:11:56 -0400

    ejercicio prepa

---
+++ ejercicioprepa.py
@@ -0,0 +1,5 @@
+#Realizar un programa que pida el nombre y edad y devuelva un saludo y el ano en que nacio
+nombre=input('Ingrese su nombre: ')
+edad=input('Ingrese su edad: ')
+edad=int(edad)
+print(f'Hola {nombre}, su ano de nacimiento es {2025-edad}')
